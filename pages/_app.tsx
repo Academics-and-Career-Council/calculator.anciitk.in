95a8819535c31e99ba7a615f0272f62d0f00672f
faheem <nfaheem20@iitk.ac.in>
2023-02-02 13:01:18 +0530

    removed verify from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 // import { RecoilRoot } from "recoil";
 import { BrowserRouter, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import Verify from "../pages/verify";
 import { Route, redirect as DomRedirect } from "react-router-dom";
 
 function MyApp({ Component, pageProps }: AppProps) {
