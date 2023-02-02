65a5dfb5cd49eff3e05d6e56deb6967c41038900
Faheem-Nizar <nfaheem20@iitk.ac.in>
2023-02-02 13:25:30 +0530

    bracket added

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
   if (session !== undefined) {
     setSession(session);
     router.push("/dashboard");
+  }
 
   const sessiondata = useRecoilValue(recoilSessionState);
 
